5eb0fa748ae7d31e85f45c2567a9719ead953320
Freddie <freddiestoddart000@gmail.com>
2017-09-13 11:55:56 +1200

    Removed old function

--- main.go
+++ main.go
@@ -69,28 +69,3 @@ func main() {
 
 	http.ListenAndServe(":8080", m)
 }
-
-func downloadFile(filepath string, url string) (err error) {
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Writer the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
