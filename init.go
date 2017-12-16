b5a9ec80dbb5cdc73d1186deecc35d813d4b00f0
Freddie Stoddart <freddiestoddart000@gmail.com>
2017-12-16 17:23:41 +1300

    Fixed alias for unpack
    Shrunk main.go
    
    added
      init.go
      downloadHandler.go

---
+++ init.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	pflag.Bool("RestoreAssets", false, "Do you want to unpack static files to modify")
+	pflag.Bool("Unpack", false, "Do you want to unpack static files to modify")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	viper.SetDefault("Port", ":8080")
+	viper.RegisterAlias("RestoreAssets", "Unpack")
+	viper.SetConfigName("config")        // name of config file (without extension)
+	viper.AddConfigPath("/etc/godown/")  // path to look for the config file in
+	viper.AddConfigPath("$HOME/.godown") // call multiple times to add many search paths
+	viper.AddConfigPath(".")             // optionally look for config in the working directory
+	err := viper.ReadInConfig()          // Find and read the config file
+	if err != nil {                      // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+}
--- main.go
+++ main.go
@@ -2,32 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/url"
-	"os"
-	"time"
 
-	"github.com/cavaliercoder/grab"
 	"github.com/gorilla/mux"
-	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
-
-func init() {
-	flag.Bool("RestoreAssets", false, "Do you want to unpack static files to modify")
-	flag.Parse()
-	viper.BindPFlags(flag.CommandLine)
-	viper.RegisterAlias("RestoreAssets", "Unpack")
-	viper.SetConfigName("config")        // name of config file (without extension)
-	viper.AddConfigPath("/etc/godown/")  // path to look for the config file in
-	viper.AddConfigPath("$HOME/.godown") // call multiple times to add many search paths
-	viper.AddConfigPath(".")             // optionally look for config in the working directory
-	err := viper.ReadInConfig()          // Find and read the config file
-	if err != nil {                      // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %s", err))
-	}
-}
 
 func main() {
 
@@ -48,64 +27,7 @@ func main() {
 		m.PathPrefix("/").Handler(http.FileServer(assetFS()))
 	}
 
-	fmt.Println("Server running on port 8080")
+	fmt.Printf("Server running @ http//localhost%s\n", viper.GetString("Port"))
 
-	http.ListenAndServe(":8080", m)
-}
-
-func downloadHandler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("Request Recieved")
-
-	body, _ := ioutil.ReadAll(r.Body)
-
-	downloadURL := string(body)
-
-	_, err := url.ParseRequestURI(downloadURL)
-
-	if err == nil {
-
-		filePath := viper.GetString("DownloadFolder")
-
-		client := grab.NewClient()
-		req, _ := grab.NewRequest(filePath, downloadURL)
-
-		// sum, err := hex.DecodeString("12767bda45b430d66e538a8780587260427935f7513479371dc2a884723ae410")
-		// if err != nil {
-		// 	panic(err)
-		// }
-		//
-		// req.SetChecksum(sha256.New(), sum, true)
-
-		fmt.Printf("Downloading %v...\n", req.URL())
-		resp := client.Do(req)
-		fmt.Printf("  %v\n", resp.HTTPResponse.Status)
-
-		// start UI loop
-		t := time.NewTicker(500 * time.Millisecond)
-		defer t.Stop()
-
-	Loop:
-		for {
-			select {
-			case <-t.C:
-				fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
-					resp.BytesComplete(),
-					resp.Size,
-					100*resp.Progress())
-
-			case <-resp.Done:
-				// download is complete
-				break Loop
-			}
-		}
-
-		// check for errors
-		if err := resp.Err(); err != nil {
-			fmt.Fprintf(os.Stderr, "Download failed: %v\n", err)
-			//os.Exit(1)
-		}
-
-		fmt.Printf("Download saved to %v \n", resp.Filename)
-	}
+	http.ListenAndServe(viper.GetString("Port"), m)
 }
