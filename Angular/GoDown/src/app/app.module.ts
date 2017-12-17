2c804cd3ac6b3cdca416f89f760e873870cfb0ad
Freddie Stoddart <freddiestoddart000@gmail.com>
2017-12-17 19:59:58 +1300

    Starting downloads page

--- Angular/GoDown/src/app/app.module.ts
+++ Angular/GoDown/src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
+import { DataService } from './data.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { DownloadsComponent } from './downloads/downloads.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    DownloadsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Angular/GoDown/src/app/downloads/downloads.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import { MatMenuModule } from '@angular/material/'
+
+@Component({
+  selector: 'app-downloads',
+  templateUrl: './downloads.component.html',
+  styleUrls: ['./downloads.component.scss']
+})
+export class DownloadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
